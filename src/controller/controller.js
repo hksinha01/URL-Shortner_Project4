5e6dd404f9aec4f0e4a2af352c7dd9c61b00769b
hksinha01 <hksinha19012001@gmail.com>
2022-04-07 23:33:44 +0530

    phase 2

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -2,6 +2,34 @@ const urlModel = require("../model/urlModel")
 const validUrl = require('valid-url')
 const shortid = require('shortid')
 
+
+//-----------------------------redis for cache--------------------------------------------------------
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+    19651,
+  "redis-19651.c53.west-us.azure.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("WnaT0ldWFGhcYkvzJvsf0UPrUX2H7VJm", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+//....................................................................................................................
+
 const isValidReqBody = function (requestBody) {
 
     return Object.keys(requestBody).length > 0
@@ -25,10 +53,6 @@ const shorten = async function (req, res) {
         const baseUrl = "http://localhost:3000";
         const data = req.body;
 
-        if(isValidReqBody(req.query)){
-            return res.status(400).send({status : false,msg : "Query Should Not be Present"})
-        }
-
         if (!isValidReqBody(data)) {
             return res.status(400).send({ status: false, msg: "Please Enter Valid Data" })
         }
@@ -45,12 +69,22 @@ const shorten = async function (req, res) {
 
         if (!validUrl.isUri(baseUrl)) {
             console.log("invalid base url")
-            return res.status(400).send({ status: false, msg: 'Invalid base URL' })
+            return res.status(401).send({ status: false, msg: 'Invalid base URL' })
         }
 
-        const short = shortid.generate().toLowerCase().trim()
+        const short = shortid.generate()
 
         if (validUrl.isUri(longUrl)) {
+            
+            
+            let checkforUrl = await GET_ASYNC(`${longUrl}`)
+           if (checkforUrl) {
+           console.log("This is from cache")
+            return res.status(200).send({ status: true, "data": JSON.parse(checkforUrl) })
+          }
+    //---FETCH THE DATA IN MONGO DB IF IT IS NOT PRESENT IN CACHE
+
+
     
             let url =await urlModel.findOne({ longUrl}).select({createdAt:0,updatedAt:0,__v:0})
             if (url) {
@@ -64,11 +98,13 @@ const shorten = async function (req, res) {
             const final = {
                 longUrl : CreatedUrl.longUrl, shortUrl:CreatedUrl.shortUrl, urlCode : CreatedUrl.urlCode
             }
-            res.status(201).send({ status: true, msg: "Short Url Created Succesfully", data: final })
+            await SET_ASYNC(`${longUrl}`, JSON.stringify(CreatedUrl))
+
+           return res.status(201).send({ status: true, msg: "Short Url Created Succesfully", data: final })
 
         }
         else {
-            res.status(400).send({ status: false, msg: "Invalid Long Url" })
+           return res.status(401).send({ status: false, msg: "Invalid Long Url" })
         }
     }
     catch (err) {
@@ -81,23 +117,29 @@ const shorten = async function (req, res) {
 const urlCode = async function(req,res){
     try{
         const code = req.params.urlCode;
-        if(!isValid(code.trim().toLowerCase())){
+
+        if(!isValid(code)){
             return res.status(400).send({status : false , msg : "Please pass Url Code In Params"})
         }
 
-        if(isValidReqBody(req.body)){
-            return res.status(400).send({status : false,msg : "Body Should Not be Present"})
+        let cahcedUrlCode = await GET_ASYNC(`${req.params.urlCode}`)
+
+        if(cahcedUrlCode) {
+            parseData = JSON.parse(cahcedUrlCode)
+            console.log("THis is from cache")
+            return res.status(302).redirect(parseData.longUrl)
         }
-        if(isValidReqBody(req.query)){
-            return res.status(400).send({status : false,msg : "Query Should Not be Present"})
-        }
+
         const data = await urlModel.findOne({urlCode: code}).select({createdAt:0,updatedAt:0,__v:0})
-        if(data){
-            return res.status(302).redirect(data.longUrl)
-           
+        if(!data){
+            return res.status(404).send({status:false,msg : "No Such URL found"})        
         }
+
         else{
-        return res.status(404).send({status:false,msg : "No Such URL found"})
+            await SET_ASYNC(`${req.params.urlCode}`, JSON.stringify(data))
+
+            return res.status(302).redirect(data.longUrl)
+
         }
     }
     catch(error){
